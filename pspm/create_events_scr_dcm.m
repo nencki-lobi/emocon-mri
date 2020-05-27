23ea4b2d5312f987a7d20119a4a8f98785be5e6e
Michał Szczepanik <m.szczepanik@nencki.edu.pl>
2020-05-27 14:13:47 +0200

    Improve create_events_scr_dcm.m

--- pspm/create_events_scr_dcm.m
+++ pspm/create_events_scr_dcm.m
@@ -13,70 +13,37 @@ SOA = 7.5;
 
 subject = "Aflqrp";
 
-% OFL 
-event_tab = read_event_table(fullfile(...
-    bids_root, ...
-    "sub-" + subject, ...
-    "func", ...
-    "sub-" + subject + "_task-ofl_events.tsv"));
+% OFL
+event_tab = readtable(...
+    fullfile(bids_root, ...
+        "sub-" + subject, ...
+        "func", ...
+        "sub-" + subject + "_task-ofl_events.tsv"), ...
+    'FileType', 'text', 'TextType', 'string');
 
 cs_onset = event_tab{startsWith(event_tab.trial_type, 'cs_'), 'onset'};
 us_onset = cs_onset + SOA;
 
+events = cell(1, 2);
 events{1} = [cs_onset us_onset];
 events{2} = us_onset;
 
-% TODO save in the right place
-save('/Volumes/Transcend/emocon_mri/pspm/events_scr_dcm/sub-Aflqrp_task-ofl_events.mat', ...
+save(fullfile(pspm_root, "events_scr_dcm/sub-" + subject + '_task-ofl_events.mat'), ...
     'events');
 
 % DE
-events = cell(1);  % avoid copying OFL events
-event_tab = read_event_table(fullfile(...
-    bids_root, ...
-    "sub-" + subject, ...
-    "func", ...
-    "sub-" + subject + "_task-de_events.tsv"));
+event_tab = readtable(...
+    fullfile(bids_root, ...
+        "sub-" + subject, ...
+        "func", ...
+        "sub-" + subject + "_task-de_events.tsv"), ...
+    'FileType', 'text', 'TextType', 'string');
 
 cs_onset = event_tab{startsWith(event_tab.trial_type, 'cs_'), 'onset'};
-us_onset = cs_onset + SOA  % no US, but keep time window consistent
+us_onset = cs_onset + SOA;  % no US, but keep time window consistent
 
+events = cell(1, 1);
 events{1} = [cs_onset, us_onset];
 
-% --- cut below --- 
-% probably I'll put it in a function which returns a struct ?\_(?)_/?
-
-trial_idx = 1;
-idx_cspr = [];
-idx_csp = [];
-idx_csm = [];
-
-for n = 1:height(event_tab)
-    if event_tab.trial_type(n) == "cs_plus" && event_tab.trial_type(n+1) == "us"
-        % last cs+ always reinforced, so won't go OOB
-        idx_cspr(end+1) = trial_idx;
-        trial_idx = trial_idx + 1;
-    elseif event_tab.trial_type(n) == "cs_plus"
-        idx_csp(end+1) = trial_idx;
-        trial_idx = trial_idx + 1;
-    elseif event_tab.trial_type(n) == "cs_minus"
-        idx_csm(end+1) = trial_idx;
-        trial_idx = trial_idx + 1;
-    end
-end
-
-weights_us = zeros(length(cs_onset), 1);  % shouldn't be horizontal?
-weights_us(idx_cspr) = 1;
-weights_us(idx_csp) = -1;
-
-weights_cs = ones(length(cs_onset), 1);
-weights_cs(idx_csm) = -1;
-
-% --- cut below ---
-cs_trials = event_tab{startsWith(event_tab.trial_type, "cs"), "trial_type"}';
-idx_csp = find(cs_trials == "cs_plus");
-idx_csm = find(cs_trials == "cs_minus");
-
-weights_cs = ones(1, length(cs_trials));
-weights_cs(1, idx_csm) = -1;
-
+save(fullfile(pspm_root, "events_scr_dcm/sub-" + subject + '_task-de_events.mat'), ...
+    'events');
