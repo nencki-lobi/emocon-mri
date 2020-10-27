961bc4b289451557fa966226539c7fce134e239c
Michał Szczepanik <m.szczepanik@nencki.edu.pl>
2020-10-27 14:08:19 +0100

    Change rTPJ VOI definition to Neurosynth / spherical
    Use spherical 8 mm ROI centered around peak coordinates from
    Neurosynth for term 'TPJ' (global maximum for association test).

--- spm/mot_out/voi_ofl_rTPJ.m
+++ spm/mot_out/voi_ofl_rTPJ.m
@@ -7,32 +7,27 @@ roi_dir = fullfile(my_config.spm.root, 'roi');
 % select all 1st level models for OFL
 files = dir(fullfile(analysis_dir, 'first_level', '*_ofl', 'SPM.mat'));
 
-% fetch VOI definition if not present
-% should probably be moved elsewhere (python script with all rois?)
-local_path = fullfile(roi_dir, 'seed_rTPJ_vox200.nii');
-mask_url = 'https://neurovault.org/media/images/2462/seed_rTPJ_vox200.nii.gz';
-if ~ isfile(local_path)
-    gunzip(mask_url, roi_dir)
-end
-
 % batch setup
 nrun = size(files, 1);
 jobfile = {'voi_ofl_rTPJ_job.m'};
 jobs = repmat(jobfile, 1, nrun);
-inputs = cell(5, nrun);
+inputs = cell(6, nrun);
 
 % voi definitions - shared by all subjects
-voi_mask = fullfile(roi_dir, 'seed_rTPJ_vox200.nii,1');
 contrast = [3, 1, 1; 4, 1, -1];
 ppi_name = 'RTPJxUSDIFF';
 
 for crun = 1:nrun
     model = fullfile(files(crun,1).folder, files(crun,1).name);
+    centre = [58 -50 14];  % peak for term 'tpj' on Neurosynth
+    radius = 8;
+    
     inputs{1, crun} = cellstr(model); % Volume of Interest: Select SPM.mat - cfg_files
-    inputs{2, crun} = cellstr(voi_mask); % Volume of Interest: Image file - cfg_files
-    inputs{3, crun} = cellstr(model); % Physio/Psycho-Physiologic Interaction: Select SPM.mat - cfg_files
-    inputs{4, crun} = contrast; % Physio/Psycho-Physiologic Interaction:  Input variables and contrast weights - cfg_entry
-    inputs{5, crun} = ppi_name; % Physio/Psycho-Physiologic Interaction: Name of PPI - cfg_entry
+    inputs{2, crun} = centre; % Volume of Interest: Centre - cfg_entry
+    inputs{3, crun} = radius; % Volume of Interest: Radius - cfg_entry
+    inputs{4, crun} = cellstr(model); % Physio/Psycho-Physiologic Interaction: Select SPM.mat - cfg_files
+    inputs{5, crun} = contrast; % Physio/Psycho-Physiologic Interaction:  Input variables and contrast weights - cfg_entry
+    inputs{6, crun} = ppi_name; % Physio/Psycho-Physiologic Interaction: Name of PPI - cfg_entry
 end
 spm('defaults', 'FMRI');
 spm_jobman('run', jobs, inputs{:});
--- spm/mot_out/voi_ofl_rTPJ_job.m
+++ spm/mot_out/voi_ofl_rTPJ_job.m
@@ -1,14 +1,15 @@
 %-----------------------------------------------------------------------
-% Job saved on 28-Aug-2020 12:50:35 by cfg_util (rev $Rev: 7345 $)
-% spm SPM - SPM12 (7487)
+% Job saved on 27-Oct-2020 13:52:49 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.util.voi.spmmat = '<UNDEFINED>';
 matlabbatch{1}.spm.util.voi.adjust = 1;
 matlabbatch{1}.spm.util.voi.session = 1;
 matlabbatch{1}.spm.util.voi.name = 'rTPJ';
-matlabbatch{1}.spm.util.voi.roi{1}.mask.image = '<UNDEFINED>';
-matlabbatch{1}.spm.util.voi.roi{1}.mask.threshold = 0.5;
+matlabbatch{1}.spm.util.voi.roi{1}.sphere.centre = '<UNDEFINED>';
+matlabbatch{1}.spm.util.voi.roi{1}.sphere.radius = '<UNDEFINED>';
+matlabbatch{1}.spm.util.voi.roi{1}.sphere.move.fixed = 1;
 matlabbatch{1}.spm.util.voi.expression = 'i1';
 matlabbatch{2}.spm.stats.ppi.spmmat = '<UNDEFINED>';
 matlabbatch{2}.spm.stats.ppi.type.ppi.voi(1) = cfg_dep('Volume of Interest:  VOI mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','voimat'));
