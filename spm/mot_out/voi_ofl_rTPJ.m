98837dfc0bd70484ef8da5ab963f135136372bdd
Michał Szczepanik <m.szczepanik@nencki.edu.pl>
2020-10-27 17:28:36 +0100

    Add subject mask to spherical rTPJ VOI
    Otherwise crashes for 1 subject (ZTLXHI?) because there is a minimal
    mismatch.

--- spm/mot_out/voi_ofl_rTPJ.m
+++ spm/mot_out/voi_ofl_rTPJ.m
@@ -11,7 +11,7 @@ files = dir(fullfile(analysis_dir, 'first_level', '*_ofl', 'SPM.mat'));
 nrun = size(files, 1);
 jobfile = {'voi_ofl_rTPJ_job.m'};
 jobs = repmat(jobfile, 1, nrun);
-inputs = cell(6, nrun);
+inputs = cell(7, nrun);
 
 % voi definitions - shared by all subjects
 contrast = [3, 1, 1; 4, 1, -1];
@@ -19,15 +19,17 @@ ppi_name = 'RTPJxUSDIFF';
 
 for crun = 1:nrun
     model = fullfile(files(crun,1).folder, files(crun,1).name);
+    subject_mask = fullfile(files(crun,1).folder, 'mask.nii,1');
     centre = [58 -50 14];  % peak for term 'tpj' on Neurosynth
     radius = 8;
     
     inputs{1, crun} = cellstr(model); % Volume of Interest: Select SPM.mat - cfg_files
     inputs{2, crun} = centre; % Volume of Interest: Centre - cfg_entry
     inputs{3, crun} = radius; % Volume of Interest: Radius - cfg_entry
-    inputs{4, crun} = cellstr(model); % Physio/Psycho-Physiologic Interaction: Select SPM.mat - cfg_files
-    inputs{5, crun} = contrast; % Physio/Psycho-Physiologic Interaction:  Input variables and contrast weights - cfg_entry
-    inputs{6, crun} = ppi_name; % Physio/Psycho-Physiologic Interaction: Name of PPI - cfg_entry
+    inputs{4, crun} = cellstr(subject_mask); % Volume of Interest: Image file - cfg_files
+    inputs{5, crun} = cellstr(model); % Physio/Psycho-Physiologic Interaction: Select SPM.mat - cfg_files
+    inputs{6, crun} = contrast; % Physio/Psycho-Physiologic Interaction:  Input variables and contrast weights - cfg_entry
+    inputs{7, crun} = ppi_name; % Physio/Psycho-Physiologic Interaction: Name of PPI - cfg_entry
 end
 spm('defaults', 'FMRI');
 spm_jobman('run', jobs, inputs{:});
--- spm/mot_out/voi_ofl_rTPJ_job.m
+++ spm/mot_out/voi_ofl_rTPJ_job.m
@@ -1,6 +1,6 @@
 %-----------------------------------------------------------------------
-% Job saved on 27-Oct-2020 13:52:49 by cfg_util (rev $Rev: 7345 $)
-% spm SPM - SPM12 (7771)
+% Job saved on 27-Oct-2020 17:16:08 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7487)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.util.voi.spmmat = '<UNDEFINED>';
@@ -10,7 +10,9 @@ matlabbatch{1}.spm.util.voi.name = 'rTPJ';
 matlabbatch{1}.spm.util.voi.roi{1}.sphere.centre = '<UNDEFINED>';
 matlabbatch{1}.spm.util.voi.roi{1}.sphere.radius = '<UNDEFINED>';
 matlabbatch{1}.spm.util.voi.roi{1}.sphere.move.fixed = 1;
-matlabbatch{1}.spm.util.voi.expression = 'i1';
+matlabbatch{1}.spm.util.voi.roi{2}.mask.image = '<UNDEFINED>';
+matlabbatch{1}.spm.util.voi.roi{2}.mask.threshold = 0.5;
+matlabbatch{1}.spm.util.voi.expression = 'i1 & i2';
 matlabbatch{2}.spm.stats.ppi.spmmat = '<UNDEFINED>';
 matlabbatch{2}.spm.stats.ppi.type.ppi.voi(1) = cfg_dep('Volume of Interest:  VOI mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','voimat'));
 matlabbatch{2}.spm.stats.ppi.type.ppi.u = '<UNDEFINED>';
