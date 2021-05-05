a64e5ccad51753fe6fae99d1c992b850b0e17f79
Michał Szczepanik <m.szczepanik@nencki.edu.pl>
2021-05-05 18:59:37 +0200

    Add second level scripts for OFL (obsUR)

---
+++ spm/complete/second_level_ofl_basic/ttest_1s.m
@@ -0,0 +1,19 @@
+% Second level 1 sample t-test (obs UR)
+
+my_config = ini2struct('../../../config.ini');
+analysis_dir = fullfile(my_config.spm.root, 'complete');
+
+files = dir(fullfile(analysis_dir, 'first_level', '*_ofl', 'con_0005.nii'));
+con_paths = fullfile({files.folder}', {files.name}');
+
+jobfile = {'ttest_1s_job.m'};
+jobs = repmat(jobfile, 1, 1);
+inputs = cell(3, 1);
+
+inputs{1, 1} = cellstr(fullfile(...
+    analysis_dir, 'second_level', 'ofl_basic', 'obsUR_1sample')); % Factorial design specification: Directory - cfg_files
+inputs{2, 1} = con_paths; % Factorial design specification: Scans - cfg_files
+inputs{3, 1} = 'obs UR (pooled)'; % Contrast Manager: Name - cfg_entry
+
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
