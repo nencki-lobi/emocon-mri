79c68baf7ff82c121da17c5b41afff80d2bd9b80
Michał Szczepanik <m.szczepanik@nencki.edu.pl>
2021-05-06 13:17:40 +0200

    Clean up the python script generating ROIs
    Since now most ROIs are generated from matlab (but it is still easier
    to create those based on Harvard-Oxford atlas from python) a part of
    the python script could be deleted. Added a file docstring.

--- utils/create_roi_masks.py
+++ utils/create_roi_masks.py
@@ -1,3 +1,12 @@
+""" Create ROI masks based on the Harvard - Oxford atlas
+
+Creates a bilateral mask for the Amygdala, by thresholding the atlas
+at 70 % (matching Lindstrom, Haaker & Olsson, Neuroimage 2018).
+
+Many other masks are created from within Matlab, using the script in
+spm/complete/roi/prepare_roi.m
+"""
+
 import argparse
 import configparser
 import numpy as np
@@ -40,44 +49,4 @@ mask_amy.to_filename(str(outdir.joinpath('amy_bilateral.nii')))
 
 if args.plot:
     plotting.plot_roi(mask_amy)
-    plt.show()
-
-
-# fetch cortical atlas
-dataset = datasets.fetch_atlas_harvard_oxford('cort-maxprob-thr25-2mm')
-
-cort_labels = dataset.labels
-cort_atlas = image.load_img(dataset.maps)
-cort_atlas_data = cort_atlas.get_fdata()
-
-# create bilateral insula mask
-mask_data = np.zeros(cort_atlas_data.shape, dtype=np.int32)
-mask_data[cort_atlas_data == cort_labels.index('Insular Cortex')] = 1
-
-mask_insula = image.new_img_like(cort_atlas, mask_data)
-mask_insula.to_filename(str(outdir.joinpath('insula_bilateral.nii')))
-
-if args.plot:
-    plotting.plot_roi(mask_insula)
-    plt.show()
-
-# fetch Destrieux atlas
-dataset = datasets.fetch_atlas_destrieux_2009()
-destrieux_labels = dataset.labels
-destrieux_atlas = image.load_img(dataset.maps)
-destrieux_atlas_data = destrieux_atlas.get_fdata()
-
-# create anterior insula mask from Destrieux atlas
-# this atlas keeps labels in numpy recarray and uses bytes literals
-arr_idx = np.where(destrieux_labels.name == b'R S_circular_insula_ant')[0][0]
-img_idx = destrieux_labels.index[arr_idx]
-
-mask_data = np.zeros(destrieux_atlas_data.shape, dtype=np.int32)
-mask_data[destrieux_atlas_data == img_idx] = 1
-
-mask_ains = image.new_img_like(destrieux_atlas, mask_data)
-mask_ains.to_filename(str(outdir.joinpath('R_S_circular_insula_ant')))
-
-if args.plot:
-    plotting.plot_roi(mask_ains)
     plt.show()
